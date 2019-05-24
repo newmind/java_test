5f86336140e27b925e79a64f7bcca5253d61c170
jgkim <junggonkim@gmail.com>
2019-05-24 20:45:26 +0900

    initial

---
+++ generator/src/main/java/kr/osci/random/App.java
@@ -0,0 +1,13 @@
+package kr.osci.random;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
